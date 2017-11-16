a3363eceacc6398061cc79693ccab225806813c4
Startled Badget <badger@littlemissfun.co.uk>
2017-11-16 21:23:57 +0000

    Skeleton of an app.  no docs, not very refined but produces a PPT from a CSV

---
+++ CsvToPPT/WorkItemInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToPPT
+{
+    class WorkItemInfo
+    {
+        public string Id { get; }
+        public string Summary { get; }
+        public string Owner { get; }
+        public int Cost { get; }
+
+        public WorkItemInfo(string id, string summary, string owner, int cost)
+        {
+            this.Id = id;
+            this.Summary = summary;
+            this.Owner = owner;
+            this.Cost = cost;
+        }
+    }
+}
